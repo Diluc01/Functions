259ff5778fca26be824be2490a0d7ade4051dd56
Berlin Tejeda <berlintejeda22@gmail.com>
2024-10-07 09:46:57 -0400

    almost done

--- funtions.js
+++ funtions.js
@@ -120,3 +120,43 @@ const mappedArray = arrayOfObjects.map((currentUser) => {
 console.log(mappedArray);
 
 //fourth problem
+
+const reduceArray = arrayOfObjects.reduce((accu, currentValue) => {
+  return accu + Number(currentValue.age);
+}, 0);
+
+console.log(reduceArray);
+
+const average2 = reduceArray / arrayOfObjects.length;
+
+console.log(average2);
+
+// part 3
+
+//first problem
+let object = {
+  name: "berlin",
+  age: 25,
+  occupation: "student",
+};
+
+function incrementAge(obj) {
+  const object2 = obj.age + 1;
+  return object2;
+}
+
+const result6 = incrementAge(object);
+
+console.log(result6);
+
+//second problem
+
+function makeCopy(obj) {
+  const objCopy = obj;
+  Number(objCopy.age) + 1;
+  return objCopy;
+}
+
+const result7 = makeCopy(object);
+
+console.log(result7);
